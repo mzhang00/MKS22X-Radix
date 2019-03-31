d1826dec3ae8a7d983ae0df7e5640bb2f2e6cbcb
Michael Zhang <mzhang00@stuy.edu>
2019-03-30 20:14:46 -0400

    Update node class

--- Node.java
+++ Node.java
@@ -1,45 +1,58 @@
-public class Node{
-    private Integer data;
-    private Node next, prev;
-    public Node(){
-        data = 0;
-        next = null;
-        prev = null;
-    }
-    public Node(Integer newdata){
-        data = newdata;
-    }
-    public Node(Integer newdata, Node newnext, Node newprev){
-        data = newdata;
-        next = newnext;
-        prev = newprev;
-    }
-    public Node(Node copy){
-        data = copy.getData();
-        next = copy.next();
-        prev = copy.prev();
-    }
-    public Node next(){
-        return next;
-    }
-    public Node prev(){
-        return prev;
-    }
-    public void setNext(Node other){
-        next = other;
-    }
-    public void setPrev(Node other){
-        prev = other;
-    }
-    public Integer getData(){
-        return data;
-    }
-    public Integer setData(Integer i){
-        int temp = data;
-        data = i;
-        return temp;
-    }
-    public String toString(){
-        return "" + data;
-    }
+public class Node<E>{
+  private E data;
+  private Node next, prev;
+  
+  public Node(){
+    data = null;
+    next = null;
+    prev = null;
+  }
+  
+  public Node(E newdata){
+    data = newdata;
+    next = null;
+    prev = null;
+  }
+  
+  public Node(E newdata, Node newnext, Node newprev){
+    data = newdata;
+    next = newnext;
+    prev = newprev;
+  }
+  
+  public Node(Node copy){
+    data = copy.getData();
+    next = copy.next();
+    prev = copy.prev();
+  }
+  
+  public Node next(){
+    return next;
+  }
+  
+  public Node prev(){
+    return prev;
+  }
+  
+  public void setNext(Node other){
+    next = other;
+  }
+  
+  public void setPrev(Node other){
+    prev = other;
+  }
+  
+  public E getData(){
+    return data;
+  }
+   
+  public E setData(E i){
+    E temp = data;
+    data = i;
+    return temp;
+  }
+  
+  public String toString(){
+    return data.toString();
+  }
 }
