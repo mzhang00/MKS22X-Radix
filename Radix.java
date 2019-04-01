4ae0b3b40d2f7a3037147cd64e1489bfc74c4299
Michael Zhang <mzhang00@stuy.edu>
2019-04-01 10:17:52 -0400

    Update radix

--- Radix.java
+++ Radix.java
@@ -1,5 +1,23 @@
 public class Radix{
-  public void radixsort(int[]data){
+  public static void radixsort(int[]data){
+    @SuppressWarnings("unchecked")
+    MyLinkedList<Integer>[] list = new MyLinkedList[10];
+    int numofdigits = maxDigits(data);
     return;
+  }
+
+  private static int maxDigits(int[] data){
+    int max = 0;
+    for (int i = 0; i < data.length; i++){
+      String temp = ("" + data[i]);
+      if (temp.charAt(0) != '-' && temp.length() > max){
+        max = ("" + data[i]).length();
+      }else{
+        if (temp.charAt(0) == '-' && temp.length() - 1 > max){
+          max = ("" + data[i]).length() - 1;
+        }
+      }
+    }
+    return max;
   }
 }
