1da523daf52a521e42dd802e414eb165bcc89002
Michael Zhang <mzhang00@stuy.edu>
2019-04-02 09:53:00 -0400

    Add helper method for radix

--- Radix.java
+++ Radix.java
@@ -1,10 +1,22 @@
 public class Radix{
-  public static void radixsort(int[]data){
+  public static String[] radixsort(int[] data){
     @SuppressWarnings("unchecked")
     MyLinkedList<Integer>[] list = new MyLinkedList[10];
     int numofdigits = maxDigits(data);
-    while (numofdigits > 0){
-      
+    String[] newdata = new String[data.length];
+    for (int i = 0; i < data.length; i++){
+      newdata[i] = "" + data[i];
+    }
+    return newdata;
+    //radixH(data, list, numofdigits);
+  }
+
+  private static void radixH(int[] data, MyLinkedList<Integer>[] buckets, int digits){
+    if (digits == 0){
+      return;
+    }
+    for (int i = 0; i < data.length; i++){
+      return;
     }
   }
 
@@ -24,7 +36,10 @@ public class Radix{
   }
 
   public static void main(String[] args){
-    //int[] test = {0,1,123,-1231223, -123};
-    //System.out.println(maxDigits(test));
+    int[] test = {0,1,123,-1231223, -123};
+    String[] ans = radixsort(test);
+    for (String i : ans){
+      System.out.println(i);
+    }
   }
 }
