c49605701a3ae21611b2c993b8d44ce0a64b53e0
Michael Zhang <mzhang00@stuy.edu>
2019-04-02 10:14:22 -0400

    Update radixsort

--- Radix.java
+++ Radix.java
@@ -1,24 +1,33 @@
+import java.util.*;
+
 public class Radix{
-  public static String[] radixsort(int[] data){
+  public static void radixsort(int[] data){
     @SuppressWarnings("unchecked")
     MyLinkedList<Integer>[] list = new MyLinkedList[10];
+    for (int i = 0; i < list.length; i++){
+      list[i] = new MyLinkedList<Integer>();
+    }
     int numofdigits = maxDigits(data);
-    String[] newdata = new String[data.length];
+    /*String[] newdata = new String[data.length];
     for (int i = 0; i < data.length; i++){
       newdata[i] = "" + data[i];
-    }
-    return newdata;
-    //radixH(data, list, numofdigits);
+    }*/
+    //radixH(newdata, list, numofdigits + 1, 1);
   }
 
-  private static void radixH(int[] data, MyLinkedList<Integer>[] buckets, int digits){
-    if (digits == 0){
+/*
+  private static void radixH(String[] data, MyLinkedList<Integer>[] buckets, int maxdigits, int currentdigit){
+    if (currentdigit >= maxdigits){
       return;
     }
     for (int i = 0; i < data.length; i++){
-      return;
+      int digit = Integer.parseInt(data[i]) % (int)Math.pow(10.0, currentdigit) / (int)Math.pow(10.0, currentdigit - 1);
+      if (data[i].charAt(0) == '-'){
+        buckets[digit].addFront(Integer.parseInt(data[i]));
+      }
     }
   }
+*/
 
   private static int maxDigits(int[] data){
     int max = 0;
@@ -37,9 +46,10 @@ public class Radix{
 
   public static void main(String[] args){
     int[] test = {0,1,123,-1231223, -123};
-    String[] ans = radixsort(test);
-    for (String i : ans){
+    radixsort(test);
+    /*for (String i : ans){
       System.out.println(i);
-    }
+    }*/
+    System.out.println(386 % 1000 / 100);
   }
 }
